92d22e4737e1e0276083ec348efe92006853b727
Richard Yu <vipzhicheng@gmail.com>
2022-02-11 11:52:19 +0800

    build 20220211

--- 2022/2022-02-11/src/App.vue
+++ 2022/2022-02-11/src/App.vue
@@ -3,9 +3,24 @@
 
 import * as CodeMirror from 'codemirror';
 import 'codemirror/lib/codemirror.css';
-// import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/markdown/markdown';
+
+import 'codemirror/addon/dialog/dialog.css';
 import 'codemirror/theme/material.css';
+import 'codemirror/theme/solarized.css';
+
+import './theme/base2tone-sea-dark.css';
+
+import 'code-mirror-themes/themes/made-of-code.css'
+
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/coffeescript/coffeescript';
+import 'codemirror/mode/markdown/markdown';
+
+import 'codemirror/addon/dialog/dialog.js';
+import 'codemirror/addon/search/searchcursor.js';
+import 'codemirror/addon/edit/matchbrackets.js';
+
 import 'codemirror/keymap/vim.js';
 
 import { onMounted } from 'vue'
@@ -13,21 +28,29 @@ onMounted(() => {
   let editor = document.getElementById("editor") as HTMLTextAreaElement;
   editor.value = "";
 
+  // @ts-ignore
+  CodeMirror.commands.save = function () {
+    console.log("save");
+    alert('save')
+  };
+
   CodeMirror.fromTextArea(editor as HTMLTextAreaElement, {
     mode: 'markdown',
-    theme: 'material',
+    theme: 'markdown',
     lineNumbers: true,
     lineWrapping: true,
     indentUnit: 2,
     tabSize: 2,
-    indentWithTabs: false,
+    indentWithTabs: true,
+    showCursorWhenSelecting: true,
     keyMap: 'vim',
-    // extraKeys: {
-    //   Tab: (cm: CodeMirror.Editor) => {
-    //     const spaces = Array(cm.getOption('indentUnit') + 1).join(' ');
-    //     cm.replaceSelection(spaces);
-    //   },
-    // },
+    extraKeys: {
+      // indent with spaces
+      Tab: (cm: CodeMirror.Editor) => {
+        const spaces = Array(cm.getOption('indentUnit') + 1).join(' ');
+        cm.replaceSelection(spaces);
+      },
+    },
   });
 });
 
@@ -36,15 +59,12 @@ onMounted(() => {
 </script>
 
 <template>
-<textarea id="editor" class="w-screen h-screen"></textarea>
+<textarea id="editor"></textarea>
 </template>
 
 <style>
 .CodeMirror {
   width: 100%;
   height: 100%;
-}
-.CodeMirror-dialog-bottom {
-  margin-top: -80px;
 }
 </style>
