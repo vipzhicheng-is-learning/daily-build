5ae12aa7d6c1620fa6307b1562631c11979a16ec
vipzhicheng <vipzhicheng@gmail.com>
2022-02-17 00:01:36 +0800

    build: 20220216

---
+++ 2022/2022-02-16/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted } from "vue";
+import Prism from 'prismjs';
+import "prismjs/themes/prism-dark.css";
+const code = `function hello(){
+  console.log(Hello World)
+}
+`;
+
+
+
+onMounted(() => {
+  Prism.highlightAll()
+})
+
+</script>
+
+<template>
+<pre ><code class="language-javascript">{{code}}</code></pre>
+</template>
+
+<style>
+
+</style>
